0eca79269c61ab877cf5910f513412b40b484bd8
Harith Onigemo <onigemotosin@gmail.com>
2021-08-17 19:24:04 +0400

    Zuri Internship stage 2 exercise

---
+++ printName.py
@@ -0,0 +1,14 @@
+name = "Harith Oluwatosin Onigemo"
+
+
+# userInput = input("Type 'yes' to print my name ")
+
+def printName():
+    print(name)
+
+# if userInput.lower() == "yes":
+#     printName()
+# else:
+#     print("Exiting programme")
+
+printName()
